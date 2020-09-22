82a1569c8e4784c73a59cefb76191c222d751950
João Koritar <jakoritarleite@gmail.com>
2020-09-21 22:44:58 -0300

    build: add build for Android 11

---
+++ build/core/definitions.mk
@@ -0,0 +1,9 @@
+#
+# Adara-specific macros
+#
+
+# Include board/platform macros
+include vendor/adara/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/adara/build/core/vendor/*.mk
---
+++ build/soong/android/variables.go
@@ -0,0 +1,69 @@
+package android
+type Product_variables struct {
+	Target_init_vendor_lib struct {
+		Whole_static_libs []string
+	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
+	Target_shim_libs struct {
+		Cppflags []string
+	}
+        Target_surfaceflinger_fod_lib struct {
+		Cflags []string
+		Whole_static_libs []string
+	}
+	Target_uses_color_metadata struct {
+		Cppflags []string
+	}
+	Additional_gralloc_10_usage_bits struct {
+		Cppflags []string
+	}
+        Bootloader_message_offset struct {
+		Cflags []string
+	}
+        Qcom_um_soong_namespace struct {
+		Cflags []string
+		Header_libs []string
+	}
+        Uses_qcom_um_family struct {
+		Cflags []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_3_18_family struct {
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_4_family struct {
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_9_family struct {
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_14_family struct {
+		Header_libs []string
+		Shared_libs []string
+	}
+	Supports_extended_compress_format struct {
+		Cflags []string
+	}
+}
+
+type ProductVariables struct {
+	Target_init_vendor_lib				*string `json:",omitempty"`
+	Target_process_sdk_version_override	*string `json:",omitempty"`
+	Target_shim_libs					*string `json:",omitempty"`
+    Target_surfaceflinger_fod_lib 		*string `json:",omitempty"`
+	Target_uses_color_metadata			*bool 	`json:",omitempty"`
+	Additional_gralloc_10_usage_bits 	*string `json:",omitempty"`
+    Bootloader_message_offset			*int 	`json:",omitempty"`
+    Qcom_um_soong_namespace  			*string `json:",omitempty"`
+    Uses_qcom_um_family  				*bool	`json:",omitempty"`
+	Uses_qcom_um_3_18_family  			*bool 	`json:",omitempty"`
+	Uses_qcom_um_4_4_family  			*bool 	`json:",omitempty"`
+	Uses_qcom_um_4_9_family  			*bool 	`json:",omitempty"`
+	Uses_qcom_um_4_14_family  			*bool 	`json:",omitempty"`
+	Supports_extended_compress_format  	*bool	`json:",omitempty"`
+}
